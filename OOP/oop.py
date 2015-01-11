a8a3d10837c8ba6bb5525901bf04254c6021485e
Itamar Giron <itamar16@meet.mit.edu>
2015-01-11 18:07:55 +0200

    yo

--- OOP/oop.py
+++ OOP/oop.py
@@ -6,19 +6,32 @@ class Animal:
 		self.age = age
 
 		self.size = size
+
 	def printAll(self):
 		print(self.name)
 		print(self.color)
 		print(self.age)
 		print(self.size)
+	
+	def sleep(self):
+		print("the animal named " + self.name + " is sleeping")
+
+	def eat(self):
+		print("the animal named " + self.name + " is eating")
 
 sloth = Animal(name = "sid",color = "blue",age = 21,size = "1.6 times the cosine of awesomeness")
 
-babe = Animal(name = "babe",color = "beautyful",age = "31 but says shes 25",size = "air baloon")
+babe = Animal(name = "babe",color = "beautiful",age = "amazing",size = "air baloon")
 
 sloth.printAll()
-print()
+print("###########")
 babe.printAll()
+print("###########")
+sloth.sleep()
+sloth.eat()
+print("###########")
+babe.sleep()
+babe.eat()
 
 
 
